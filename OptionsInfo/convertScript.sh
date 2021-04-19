f714251d43495eb625db812e908ed5324f85f09a
Steve <stevenchamp11@yahoo.com>
2021-04-19 07:34:10 -0600

    Delete OptionsInfo directory

--- OptionsInfo/convertScript.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-PATH=/home/steve/anaconda3/bin:/home/steve/anaconda3/condabin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin
-
-jupyter nbconvert --to notebook --inplace --execute /home/steve/documents/flatIron/fIProject/OptionsInfo/optionDBGen.ipynb
